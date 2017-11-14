6696d336aa92c26e54e2c39a6bd326ea3b87bd9a
Витковский Станислав <Stasvitkovskiy700@gmail.com>
2017-11-14 16:43:40 +0300

    Весь проект

---
+++ Calculate C#/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double[] normal = new double[3];
+            double[] vertex1 = new double[3];
+            double[] vertex2 = new double[3];
+            double[] vertex3 = new double[3];
+            ushort count;
+            double sum = 0;
+            byte[] header = new byte[80];
+            
+            using (BinaryReader reader = new BinaryReader(File.Open("file.stl", FileMode.Open)))
+            {
+                FileStream file1 = new FileStream("test.txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(file1);
+                reader.BaseStream.Seek(84,SeekOrigin.Begin);
+                while(reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                        for (int i = 0; i < 3; i++) normal[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex1[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex2[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex3[i] = reader.ReadSingle();
+                        count = reader.ReadUInt16();
+                        sum = sum + ((((-1) * vertex3[0] * vertex2[1] * vertex1[2]) + vertex2[0] * vertex3[1] * vertex1[2] + vertex3[0] * vertex1[1] * vertex2[2] - vertex1[0] * vertex3[1] * vertex2[2] - vertex2[0] * vertex1[1] * vertex3[2] + vertex1[0] * vertex2[1] * vertex3[2]) / 6);
+                        writer.WriteLine("  facet normal " + normal[0] + " " + normal[1] + " " + normal[2]);
+                        writer.WriteLine("    outer loop");
+                        writer.WriteLine("      vertex " + vertex1[0] + " " + vertex1[1] + " " + vertex1[2]);
+                        writer.WriteLine("      vertex " + vertex2[0] + " " + vertex2[1] + " " + vertex2[2]);
+                        writer.WriteLine("      vertex " + vertex3[0] + " " + vertex3[1] + " " + vertex3[2]);
+                        writer.WriteLine("    endloop");
+                        writer.WriteLine("  endfacet");
+                }
+                sum = sum / 1000;
+                Console.WriteLine("Объём модели: " + sum + " см^3");
+                Console.WriteLine("Вес модели: " + (sum*1.25) + " грм");
+                Console.ReadLine();
+                writer.Close();
+            }
+
+        }
+    }
+}
---
+++ Calculate PHP/index.php
@@ -0,0 +1,57 @@
+﻿<?php
+//error_reporting(E_ALL ^ E_NOTICE);
+
+$filename = "C:/xampp/tmp/" .basename($_FILES['uploadfile']['tmp_name']);
+$count = 3;
+$handle = fopen($filename, "rb");
+//fseek($handle,80);
+$header = fread($handle, 80);
+$rnum = fread($handle, 4);
+$num = unpack('I', $rnum);
+print_r($num);
+
+//die();
+
+$sum = 0;
+//   while (feof($handle)==false) {
+for ($i = 0; $i< $num; $i++) {
+	$contents = fread($handle, 4);
+	$normal1 = unpack('f', fread($handle, 4));
+	$contents = fread($handle, 4);
+	$normal2 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$normal3 = unpack('f', $contents);
+		
+	$contents = fread($handle, 4);
+	$vertex11 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex12 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex13 = unpack('f', $contents);
+	
+	$contents = fread($handle, 4);
+	$vertex21 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex22 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex23 = unpack('f', $contents);
+	
+
+	$contents = fread($handle, 4);
+	$vertex31 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex32 = unpack('f', $contents);
+	$contents = fread($handle, 4);
+	$vertex33 = unpack('f', $contents);
+	
+	$atrib = fread($handle, 2);
+	
+	$sum = $sum + ((float)$vertex31 * (float)$vertex22);
+	//$sum += (float)((((-1) * $vertex31 * $vertex22 * $vertex13) + $vertex21 * $vertex32 * $vertex13 + $vertex31 * $vertex12 * $vertex23 - $vertex11 * $vertex32 * $vertex23 - $vertex21 * $vertex12 * $vertex33 + $vertex11 * $vertex22 * $vertex33) / 6);
+	
+	//$sum += (float)((((-1) * current($vertex3) * next($vertex2) * end($vertex1)) + prev($vertex2) * next($vertex3) * prev($vertex1) + prev($vertex3) * current($vertex1) * end($vertex2) - prev($vertex1) * next($vertex3) * current($vertex2) - reset($vertex2) * next($vertex1) * next($vertex3) + prev($vertex1) * next($vertex2) * current($vertex3)) / 6);
+	//$sum = $sum + ((((-1) * $vertex3[0] * $vertex2[1] * $vertex1[2]) + $vertex2[0] * $vertex3[1] * $vertex1[2] + $vertex3[0] * $vertex1[1] * $vertex2[2] - $vertex1[0] * $vertex3[1] * $vertex2[2] - $vertex2[0] * $vertex1[1] * $vertex3[2] + $vertex1[0] * $vertex2[1] * $vertex3[2]) / 6);
+} 
+fclose($handle);
+print($sum);
+?>
